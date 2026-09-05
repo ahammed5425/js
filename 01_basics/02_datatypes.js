cb3685e5e0d6cd025788def9b03503aafe74ef38
Shaik Ahammed <shaikahammed5425@gmail.com>
2026-09-05 14:15:48 +0000

    datatyopes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,25 @@
 "use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 2);
+
+console.log("Ahmed");
+
+let name = "Ahmed";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2*53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
