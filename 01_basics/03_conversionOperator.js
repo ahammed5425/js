29b6fae5aba094fe7f0f48691e6c5f271f25233a
Shaik Ahammed <shaikahammed5425@gmail.com>
2026-09-05 14:28:42 +0000

    String to number converasion

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -20,5 +20,33 @@ let isLoggedIn = 0;
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********** Operations *****************/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " morning";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log( 2 + 2 + "1");
+
+// console.log(+true);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+// console.table([num1, num2, num3]);
+
+let gameCounter = 100;
+
+console.log(gameCounter++);
+console.log(++gameCounter);
+
